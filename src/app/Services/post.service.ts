fcc11fefc21cd9eb748f379bd519e59c77a7994c
Mahender <mahenderktl750@gmail.com>
2024-10-16 14:02:24 +0530

    Base 02

--- src/app/Services/post.service.ts
+++ src/app/Services/post.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IPost } from '../Models/IPost';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -9,8 +10,10 @@ export class PostService {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    return this.http.get<{ [id: string]: IPost }>(
-      'https://angular-rxjs-declarative-posts-default-rtdb.firebaseio.com/posts.json'
-    );
+    return this.http
+      .get<{ [id: string]: IPost }>(
+        'https://angular-rxjs-declarative-posts-default-rtdb.firebaseio.com/posts.json'
+      )
+      .pipe(map((postData) => console.log(postData)));
   }
 }
